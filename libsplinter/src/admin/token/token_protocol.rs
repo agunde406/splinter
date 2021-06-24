559b67fa7e1e51954b8e67f9c0d631fbd4a2a21a
Andrea Gunderson <agunde@bitwise.io>
2021-06-24 16:57:33 -0500

    Add list_nodes and get_node_token to ListPeerAuthorizationTokens
    ListPeerAuthorizationTokens trait is a helper trait to get a list
    of tokens from a circuit or proposal. There were several cases
    that were not covered by the existing list_tokens method.
    
    list_nodes returns the tokens along with other important pieces of
    information about the node, such as its endpoints and its admin
    service id.
    
    get_node_token allows for getting a specific node's token from a
    circuit. This is used to simplify getting the local nodes expected
    authorization type.
    
    Signed-off-by: Andrea Gunderson <agunde@bitwise.io>

--- libsplinter/src/admin/token/token_protocol.rs
+++ libsplinter/src/admin/token/token_protocol.rs
@@ -16,7 +16,7 @@ use crate::admin::store::{AuthorizationType, Circuit, ProposedCircuit};
 use crate::error::InvalidStateError;
 use crate::peer::PeerAuthorizationToken;
 
-use super::ListPeerAuthorizationTokens;
+use super::{admin_service_id, ListPeerAuthorizationTokens, PeerNode};
 
 impl ListPeerAuthorizationTokens for ProposedCircuit {
     fn list_tokens(&self) -> Result<Vec<PeerAuthorizationToken>, InvalidStateError> {
@@ -40,6 +40,65 @@ impl ListPeerAuthorizationTokens for ProposedCircuit {
                 }
             })
             .collect::<Result<Vec<PeerAuthorizationToken>, InvalidStateError>>()
+    }
+
+    fn list_nodes(&self) -> Result<Vec<PeerNode>, InvalidStateError> {
+        self.members()
+            .iter()
+            .map(|member| match self.authorization_type() {
+                AuthorizationType::Trust => Ok(PeerNode {
+                    token: PeerAuthorizationToken::from_peer_id(member.node_id()),
+                    node_id: member.node_id().to_string(),
+                    endpoints: member.endpoints().to_vec(),
+                    admin_service: admin_service_id(member.node_id()),
+                }),
+                #[cfg(feature = "challenge-authorization")]
+                AuthorizationType::Challenge => {
+                    if let Some(public_key) = member.public_key() {
+                        Ok(PeerNode {
+                            token: PeerAuthorizationToken::from_public_key(public_key),
+                            node_id: member.node_id().to_string(),
+                            endpoints: member.endpoints().to_vec(),
+                            admin_service: admin_service_id(member.node_id()),
+                        })
+                    } else {
+                        Err(InvalidStateError::with_message(format!(
+                            "No public key set when circuit requries challenge \
+                             authorization: {}",
+                            self.circuit_id()
+                        )))
+                    }
+                }
+            })
+            .collect::<Result<Vec<PeerNode>, InvalidStateError>>()
+    }
+
+    fn get_node_token(
+        &self,
+        node_id: &str,
+    ) -> Result<Option<PeerAuthorizationToken>, InvalidStateError> {
+        match self
+            .members()
+            .iter()
+            .find(|member| member.node_id() == node_id)
+        {
+            Some(member) => match self.authorization_type() {
+                AuthorizationType::Trust => {
+                    Ok(Some(PeerAuthorizationToken::from_peer_id(member.node_id())))
+                }
+                #[cfg(feature = "challenge-authorization")]
+                AuthorizationType::Challenge => {
+                    if let Some(public_key) = member.public_key() {
+                        Ok(Some(PeerAuthorizationToken::from_public_key(public_key)))
+                    } else {
+                        Err(InvalidStateError::with_message(
+                            "Public key not set when required by a circuit".to_string(),
+                        ))
+                    }
+                }
+            },
+            None => Ok(None),
+        }
     }
 }
 
@@ -65,5 +124,64 @@ impl ListPeerAuthorizationTokens for Circuit {
                 }
             })
             .collect::<Result<Vec<PeerAuthorizationToken>, InvalidStateError>>()
+    }
+
+    fn list_nodes(&self) -> Result<Vec<PeerNode>, InvalidStateError> {
+        self.members()
+            .iter()
+            .map(|member| match self.authorization_type() {
+                AuthorizationType::Trust => Ok(PeerNode {
+                    token: PeerAuthorizationToken::from_peer_id(member.node_id()),
+                    node_id: member.node_id().to_string(),
+                    endpoints: member.endpoints().to_vec(),
+                    admin_service: admin_service_id(member.node_id()),
+                }),
+                #[cfg(feature = "challenge-authorization")]
+                AuthorizationType::Challenge => {
+                    if let Some(public_key) = member.public_key() {
+                        Ok(PeerNode {
+                            token: PeerAuthorizationToken::from_public_key(public_key),
+                            node_id: member.node_id().to_string(),
+                            endpoints: member.endpoints().to_vec(),
+                            admin_service: admin_service_id(member.node_id()),
+                        })
+                    } else {
+                        Err(InvalidStateError::with_message(format!(
+                            "No public key set when circuit requries challenge \
+                             authorization: {}",
+                            self.circuit_id()
+                        )))
+                    }
+                }
+            })
+            .collect::<Result<Vec<PeerNode>, InvalidStateError>>()
+    }
+
+    fn get_node_token(
+        &self,
+        node_id: &str,
+    ) -> Result<Option<PeerAuthorizationToken>, InvalidStateError> {
+        match self
+            .members()
+            .iter()
+            .find(|member| member.node_id() == node_id)
+        {
+            Some(member) => match self.authorization_type() {
+                AuthorizationType::Trust => {
+                    Ok(Some(PeerAuthorizationToken::from_peer_id(member.node_id())))
+                }
+                #[cfg(feature = "challenge-authorization")]
+                AuthorizationType::Challenge => {
+                    if let Some(public_key) = member.public_key() {
+                        Ok(Some(PeerAuthorizationToken::from_public_key(public_key)))
+                    } else {
+                        Err(InvalidStateError::with_message(
+                            "Public key not set when required by a circuit".to_string(),
+                        ))
+                    }
+                }
+            },
+            None => Ok(None),
+        }
     }
 }
